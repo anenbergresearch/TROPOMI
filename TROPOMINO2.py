7fa645a9d4f5aa7a1629fc7b74ed4c94891a6872
Anenberg Research <anenbergresearch@gmail.com>
2024-01-19 09:37:47 -0500

    removed global trends

--- TROPOMINO2.py
+++ TROPOMINO2.py
@@ -512,7 +512,7 @@ def TrendsOverTime():
 
 ##########################################################################################################################
 
-@app.addapp(title='Global Trends Over Time')
+""" @app.addapp(title='Global Trends Over Time')
 def TrendsOverTime():
 
     my_expander1 = st.expander('Trends Over Time', expanded=True)
@@ -534,7 +534,7 @@ def TrendsOverTime():
     col2.markdown("Percent Change in annual TROPOMI NO2 averaged over GHS-SMOD urban boundaries for areas with population greater than 500,000.")
     col2.text("")
     col2.markdown("<ul style='text-align: justify'>The <a href= 'https://tropomino2.us', target='_blank'>tropomino2.us</a> web site is maintained by the <a href= 'https://blogs.gwu.edu/sanenberg/', target='_blank'>Air Climate and Health Lab</a> at the Milken Institute School of Public Health at George Washington University, and is not directly affiliated with Tropomi Science Team. Data shown on the website are tropospheric vertical column amounts, are filtered to show measurements with a quality assurance flag exceeding 0.75, and are re-gridded using a methodology described in <a href= 'https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2020EF001665', target='_blank'>Goldberg et al. 2021</a>. Daily images are from the near-real-time (NRT) product and the monthly data are from the offline (OFFL) product. Data shown here are from the Version 2.2 and 2.3.1 NO2 algorithms developed by <a href= 'https://amt.copernicus.org/articles/15/2037/2022/', target='_blank'>KNMI</a>. NRT data are available on this website approximately 3 hours after the measurement. Tropomi NO2 can be downloaded from: <a href= 'http://www.tropomi.eu/data-products/nitrogen-dioxide', target='_blank'>http://www.tropomi.eu/data-products/nitrogen-dioxide</a>", unsafe_allow_html=True)
-    col2.text("")
+    col2.text("") """
 
 ##########################################################################################################################
 
